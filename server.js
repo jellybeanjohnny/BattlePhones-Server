f2ad712c0fe6c4494d49e002978d5deadf9602fc
jellybeanjohnny <mamerige@gmail.com>
2017-03-10 23:57:00 -0800

    Adds dynamic ID to keep track and easily index a connection object, adds logic to parse for certain relevant events such as a player joining.
    This is the beginning of the user presence system.

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const websocketServer = new WebSocket.Server({server});
 
+var connections = {"count" : 0};
+var nextIDTicket = 0;
+
 app.get("/", function(request, response) {
     response.send("Hello! Welcome to the BattlePhones Server");
 });
@@ -18,25 +21,42 @@ server.listen(8080, function(){
 
 websocketServer.on('connection', function connection(connection) {
     console.log("Connection to web socket server started");
+    saveConnection(connection);
 
     connection.on("message", function incoming(message) {
         handleMessage(message);
     });
 
+    connection.on("close", function close(close) {
+        handleDisconnect(close, connection);
+    });
 });
 
 function handleMessage(message) {
-    var isFirstMessage = false;
-    var userName = null; 
-    if (typeof message === 'string') {
-        if (isFirstMessage) {
-            userName = message;
-            console.log(userName + " joined the Lobby! Welcome " + userName + "!");
-        } else {
-            console.log("Server received the following message: " + message);
-        }
-
+    var jsonObject = JSON.parse(message);
+    if (jsonObject.eventType === "playerJoined") {
+        playerDidJoin(jsonObject);
     }
-    
-    
+}
+
+function handleDisconnect(close, connection) {
+    delete connections[connection.generatedID];
+    connections.count -= 1;
+    printConnections();
+}
+
+function playerDidJoin(playerInfo) {
+    console.log("%s joined the Lobby! Welcome %s!", playerInfo.username, playerInfo.username);
+}
+
+function printConnections() {
+    console.log("Connected Player Count: " + connections.count);
+}
+
+function saveConnection(connection) {
+    connections[nextIDTicket] = connection;
+    connection.generatedID = nextIDTicket;
+    nextIDTicket += 1;
+    connections.count += 1;
+    printConnections();
 }
