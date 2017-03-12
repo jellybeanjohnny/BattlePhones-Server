2a28d480f66d1e8708c09c9bb0d15bcd1486c97b
jellybeanjohnny <mamerige@gmail.com>
2017-03-12 00:00:09 -0800

    adds start of create player post request and adds body parser package

--- server.js
+++ server.js
@@ -9,18 +9,11 @@ mongoose.connect("mongodb://localhost/battlephonesdb");
 // SCHEMA SETUP. Do this in a different file later on
 var playerSchema = new mongoose.Schema({
     displayName: String,
+    iCloudID: String
 });
 
 var Player = mongoose.model("Player", playerSchema);
 
-Player.create({displayName: "Matt"}, function(error, player) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Newly created player: ");
-        console.log(player);
-    }
-});
 
 const app = express();
 
@@ -32,14 +25,20 @@ var port = process.env.PORT || 8080;
 var connections = {"count" : 0};
 var nextIDTicket = 0;
 
-app.get("/", function(request, response) {
-    response.send("Hello! Welcome to the BattlePhones Server");
-});
-
 server.listen(port, function(){
     console.log("Server started! Listening on port %d", server.address().port);
 });
 
+// Routes
+app.get("/", function(request, response) {
+    response.send("Hello! Welcome to the BattlePhones Server");
+});
+
+app.post("/newPlayer", function(request, response) {
+    console.log(request.body);
+});
+
+// Sockets
 websocketServer.on('connection', function connection(connection) {
     console.log("Connection to web socket server started");
     saveConnection(connection);
