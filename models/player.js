40eb148dc6f51fa8da255e001509dba5860f9139
jellybeanjohnny <mamerige@gmail.com>
2017-03-12 21:24:14 -0700

    associated models

--- models/player.js
+++ models/player.js
@@ -1,11 +1,16 @@
 
 const mongoose = require("mongoose");
+const PlayerStats = require("./playerStats.js");
+
 var playerSchema = new mongoose.Schema({
     displayName: String,
     uuid: {
         type: String,
         unique: true
-    }
+    },
+    attacks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Attack" }],
+    items: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
+    stats: { type: PlayerStats.schema, default: PlayerStats() }
 });
 
 module.exports = mongoose.model("Player", playerSchema);
