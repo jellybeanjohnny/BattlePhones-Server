3a29afbef7b1e3e63004d13e715c4b7afc067956
jellybeanjohnny <mamerige@gmail.com>
2017-03-14 20:31:33 -0700

    Adds action description

--- models/attack.js
+++ models/attack.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 var attackSchema = new mongoose.Schema({
-    name: { type: String, default: "Punch" },
+    name: { type: String, default: "Flail" },
     energyCost: {type: Number, default: 1},
-    damageOutput: {type: Number, default: 2}
+    damageOutput: {type: Number, default: 2},
+    actionDescription: {type: String, default: "doesn't know how to fight and flails around wildly."}
 });
 
 module.exports = mongoose.model("Attack", attackSchema);
