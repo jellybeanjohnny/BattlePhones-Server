6de73f7f263b8b95729f7516cae4b42d94f892c4
jellybeanjohnny <mamerige@gmail.com>
2017-03-14 19:25:05 -0700

    Fixing spelling

--- models/playerStats.js
+++ models/playerStats.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 var playerStatsSchema = new mongoose.Schema({
-    totalHealth:{ type: Number, default: 10 },
+    maxHealth:{ type: Number, default: 10 },
     currentHealth: { type: Number, default: 10 },
-    totalEnergy: { type: Number, default: 10 },
-    currentEnery: { type: Number, default: 10},
+    maxEnergy: { type: Number, default: 10 },
+    currentEnergy: { type: Number, default: 10},
     experiencePoints: { type: Number, default: 0 },
-    currentLevel: { type: Number, default: 1}
+    rank: { type: Number, default: 1}
 });
 
 module.exports = mongoose.model("PlayerStats", playerStatsSchema);
